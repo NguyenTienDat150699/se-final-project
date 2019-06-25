d665c1abe267d904edb92227b4e252ba16cf4c14
NguyenTienDat <34731364+NguyenTienDat150699@users.noreply.github.com>
2019-06-25 20:15:02 +0700

    Add files in ViewModel

---
+++ ViewModels/PhieuDkhpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.OleDb;
+using Models;
+using DataAccessLayer;
+using Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ViewModels
+{
+    public class PhieuDkhpViewModel : PropertyChangedBaseClass
+    {
+        public PhieuDkhpViewModel() : base()
+        {
+
+        }
+    }
+}
---
+++ ViewModels/PhieuThuHpViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.OleDb;
+using Models;
+using DataAccessLayer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class PhieuThuHpViewModel
+    {
+        public PhieuThuHpViewModel() : base()
+        {
+
+        }
+    }
+}
