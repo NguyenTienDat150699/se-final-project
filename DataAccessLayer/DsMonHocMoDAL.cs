d8872eff5c8122c2c9ffbb2b005a01fdf8c5cb4c
NguyenTienDat <34731364+NguyenTienDat150699@users.noreply.github.com>
2019-06-27 17:46:59 +0700

    CreateItem and IsMaSoExisted of some DALs

--- DataAccessLayer/DsMonHocMoDAL.cs
+++ DataAccessLayer/DsMonHocMoDAL.cs
@@ -14,5 +14,55 @@ namespace DataAccessLayer
     {
         public DsMonHocMoDAL(OleDbConnection connection) : base(connection) { }
         public DsMonHocMoDAL(string connectionString): base(connectionString) { }
+
+        public void CreateItem(DsMonHocMo dsMonHocMo)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "INSERT INTO DS_MON_HOC_MO (MaDsMonHocMo, HocKy, NamHoc) " +
+                    "VALUES (@maso, @hocky, @namhoc)",
+                    connection);
+                command.Parameters.Add("@maso", OleDbType.Numeric).Value = dsMonHocMo.MaDsMonHocMo;
+                command.Parameters.Add("@hocky", OleDbType.Numeric).Value = dsMonHocMo.HocKy;
+                command.Parameters.Add("@namhoc", OleDbType.Numeric).Value = dsMonHocMo.NamHoc;
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool IsMaSoExisted(int maDsMonHocMo)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "SELECT COUNT(MaDsMonHocMo) FROM DS_MON_HOC_MO WHERE MaDsMonHocMo=@maso", connection);
+                command.Parameters.Add("@maso", OleDbType.Numeric).Value = maDsMonHocMo;
+                OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter(command);
+                oleDbDataAdapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return int.Parse(dataTable.Rows[0][0].ToString()) == 0 ? false : true;
+        }
     }
 }
--- DataAccessLayer/SinhVienDAL.cs
+++ DataAccessLayer/SinhVienDAL.cs
@@ -14,5 +14,60 @@ namespace DataAccessLayer
     {
         public SinhVienDAL(OleDbConnection connection) : base(connection) { }
         public SinhVienDAL(string connectionString) : base(connectionString) { }
+
+        public void CreateItem(SinhVien sinhVien)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "INSERT INTO SINH_VIEN (MaSo, HoTen, NgaySinh, GioiTinh, DiaChi, QueQuan, DoiTuong, NganhHoc) " +
+                    "VALUES (@maso, @hoten, @ngaysinh, @gioitinh, @diachi, @quequan, @doituong, @nganhhoc)",
+                    connection);
+                command.Parameters.Add("@maso", OleDbType.Numeric).Value = sinhVien.MaSo;
+                command.Parameters.Add("@hoten", OleDbType.BSTR).Value = sinhVien.HoTen;
+                command.Parameters.Add("@ngaysinh", OleDbType.Date).Value = sinhVien.NgaySinh;
+                command.Parameters.Add("@gioitinh", OleDbType.Boolean).Value = sinhVien.GioiTinh;
+                command.Parameters.Add("@diachi", OleDbType.BSTR).Value = sinhVien.DiaChi;
+                command.Parameters.Add("@quequan", OleDbType.Numeric).Value = sinhVien.QueQuan;
+                command.Parameters.Add("@doituong", OleDbType.Numeric).Value = sinhVien.DoiTuong;
+                command.Parameters.Add("@nganhhoc", OleDbType.Numeric).Value = sinhVien.NganhHoc;
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool IsMaSoExisted(int maSo)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                OleDbCommand command = new OleDbCommand(
+                    "SELECT COUNT(MaSo) FROM SINH_VIEN WHERE MaSo=@maso", connection);
+                command.Parameters.Add("@maso", OleDbType.Numeric).Value = maSo;
+                OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter(command);
+                oleDbDataAdapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return int.Parse(dataTable.Rows[0][0].ToString()) == 0 ? false : true;
+        }
     }
 }
